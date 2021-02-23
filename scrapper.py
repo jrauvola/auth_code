0158bad43a937b75237ec7eaff0517e76a2ee886
msaguirr <madison.aguirre@hotmail.com>
2021-02-22 16:55:42 -0800

    fuckn shit up

--- scrapper.py
+++ scrapper.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import statistics as st
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
@@ -9,18 +10,130 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id='e2d254e0598f4ff49c5ce9
                                                scope="user-library-read"))
 
 def songs():
-    results = sp.current_user_saved_tracks(limit=10)
+    results = sp.current_user_saved_tracks(limit=1)
     for idx, item in enumerate(results['items']):
         track = item['track']
     audio_analysis(track["id"])
-    #audio_features(track["id"])
-        #print(idx, track['artists'][0]['name'], " – ", track['name'])
+    print(idx, track['artists'][0]['name'], " – ", track['name'])
+    audio_features(track["id"])
 
 def playlist():
-    results = sp.current_user_playlists(limit = 10)
+    results = sp.current_user_playlists(limit = 1)
     for idx, playlist in enumerate(results['items']):
         playlist2 = playlist['name']
         print(idx, playlist2)
+
+def get_common_related_artists(artists):
+    all_related = {}
+    for a_id in artists:
+        related_artists = sp.artist_related_artists(a_id)
+        for artist in related_artists['artists']:
+            if artist['id'] in all_related.keys():
+                all_related[artist['id']] += 1
+            else:
+                all_related[artist['id']] = 1
+    all_related = {k: v for k, v in sorted(all_related.items(), key = lambda item: item[1], reverse=True)}
+    not_on_playlist = {}
+    for (k, v) in all_related.items():
+        if (v > 1) and (k not in artists):
+            print(k,v)
+            print(sp.artist(k)['name'], v)
+            print()
+            #common_related[k] = v
+
+def playlist_artists(): 
+    #note: there's no way to get genre of a song...... wtf?
+    results = sp.current_user_playlists(limit = 1)
+    p_ids = get_items(results['items'])
+    p_id = p_ids[0]
+    tracks = sp.playlist_tracks(p_id)
+    artists = []
+    for track in tracks['items']:
+        item = track['track']
+        for artist in item['artists']:
+            if artist['id'] not in artists:
+                artists.append(artist['id'])
+                print(artist['name'], artist['id'])
+    print(artists)
+    return artists
+
+def playlist_tracks(): 
+    #note: there's no way to get genre of a song...... wtf?
+    results = sp.current_user_playlists(limit = 1)
+    p_ids = get_items(results['items'])
+    p_id = p_ids[0]
+    #print_tracks('4sJBQ3gA3wjVN8REraatpo') #one-song playlist, for testing sake
+    tracks = sp.playlist_tracks(p_id)
+    t_ids = []
+    for track in tracks['items']:
+        item = track['track']
+        t_ids.append(item['id'])
+        print('Song name:        ', item['name'])
+        print('Artist name(s):   ', item['artists'][0]['name'])
+        if len(item['artists']) > 1:
+            for artist in item['artists'][1:]:
+                print("                  ", artist['name'])
+    print()
+    stats_magic(t_ids)
+
+
+#helper function, don't remember what it does, im tired
+def print_results(results):
+    for item in results:
+        if str(item) == 'items':
+            print("Playlists:" + "\n")
+            print_items(results[item])
+            print()
+        else:
+            print(str(item) + ": " + str(results[item]))
+            print()
+
+#prints playlist(s)
+def print_items(items):
+    for item in items:
+        print("name: " + item['name'])
+        for elem in item:
+            if elem != 'name':
+                print(str(elem) + ": " + str(item[elem]))
+        print()
+
+#returns list of playlist ids
+def get_items(items):
+    ids = []
+    for item in items:
+        ids.append(item['id'])
+    return ids
+
+
+def pretty_audio_features(id):
+    results = sp.audio_features(id)
+    for idx, audio in enumerate(results):
+        for attr in audio:
+            print(attr, audio[attr])
+        print()
+
+def audio_features_stats(id):
+    results = sp.audio_features(id)
+    attr_stats = {}
+    for idx, audio in enumerate(results):
+        for attr in audio:
+            if attr in attr_stats.keys():
+                attr_stats[attr].append(audio[attr])
+            else:
+                attr_stats[attr] = [audio[attr]]
+    ret_stats = {}
+    for key in attr_stats.keys():
+        if key not in ["type", "id", "uri", "track_href", "analysis_url", "duration_ms"]:
+            ret_stats[key] = attr_stats[key] #just cleaning the clutter here
+    return ret_stats
+
+def stats_magic(t_ids):
+    data = audio_features_stats(t_ids)
+    for attr in data.keys():
+        if attr not in ["loudness", "key", "mode", "time_signature", "instrumentalness"]: 
+            #can't use harmonic mean for these. int avg/range for key, mode for mode and t_s
+            print(attr, data[attr], st.harmonic_mean(data[attr]))
+            print()
 
 # 
 # audio analysis for project - titles include 
@@ -58,8 +171,11 @@ def audio_features(id):
 # reshape method - tenserflow 
 
 def main():
+    #playlist_tracks()
+    get_common_related_artists(playlist_artists())
+    #print(sp.recommendation_genre_seeds())
     #playlist()
-    songs()
+    #songs()
 
 
 if __name__ == "__main__":
