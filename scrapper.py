1584ac20c8171e64158b3a6c4985fab6b7fdc3aa
msaguirr <madison.aguirre@hotmail.com>
2021-02-23 21:35:30 -0800

    read this code at your own risk

--- scrapper.py
+++ scrapper.py
@@ -13,7 +13,7 @@ def songs():
     results = sp.current_user_saved_tracks(limit=1)
     for idx, item in enumerate(results['items']):
         track = item['track']
-    audio_analysis(track["id"])
+    #audio_analysis(track["id"])
     print(idx, track['artists'][0]['name'], " – ", track['name'])
     audio_features(track["id"])
 
@@ -23,23 +23,58 @@ def playlist():
         playlist2 = playlist['name']
         print(idx, playlist2)
 
-def get_common_related_artists(artists):
+#this is hecka spaghetti code so just ask me if you wanna know wtf it does
+def get_common_related_artists(artists, limit=None):
+    print("Calculating results...\n")
     all_related = {}
     for a_id in artists:
         related_artists = sp.artist_related_artists(a_id)
         for artist in related_artists['artists']:
             if artist['id'] in all_related.keys():
-                all_related[artist['id']] += 1
+                all_related[artist['id']] = round(all_related[artist['id']] + artists[a_id], 4)
             else:
-                all_related[artist['id']] = 1
+                all_related[artist['id']] = artists[a_id]
+    
     all_related = {k: v for k, v in sorted(all_related.items(), key = lambda item: item[1], reverse=True)}
     not_on_playlist = {}
+    multiple_recs = {} 
+    
     for (k, v) in all_related.items():
-        if (v > 1) and (k not in artists):
-            print(k,v)
-            print(sp.artist(k)['name'], v)
-            print()
-            #common_related[k] = v
+        if k not in artists:
+            not_on_playlist[k] = v
+    magic_number = 1/max(not_on_playlist.values())
+    for (k, v) in not_on_playlist.items():
+        if v*magic_number > .66:
+            multiple_recs[k] = v
+    sorted_recs = sorted(multiple_recs.items(), key = lambda item: item[1], reverse=True)
+    if limit:
+        sorted_recs = sorted_recs[:limit]
+    for (k, v) in sorted_recs:
+        print(sp.artist(k)['name'])
+        print("Compatibility Score:", str(round(v*magic_number*100, 2)) + '%\n')
+    return multiple_recs
+
+#due to scoring becoming > 100%, this will only account for the first artist listed on each track
+#add epsilon or some shit to round
+def weighted_playlist_artists():
+    print("Getting artists...")
+    results = sp.current_user_playlists(limit = 1)
+    p_ids = get_items(results['items'])
+    p_id = p_ids[0]
+    tracks = sp.playlist_tracks(p_id)
+    artists = {}
+    for track in tracks['items']:
+        item = track['track']
+        artist = item['artists'][0]
+        if artist['id'] in artists.keys():
+            artists[artist['id']] += 1
+        else:
+            artists[artist['id']] = 1
+    test_sum = 0
+    for a in artists:
+        artists[a] = round((artists[a]/(count_songs_on_playlist())), 4)
+        test_sum += artists[a]
+    return artists
 
 def playlist_artists(): 
     #note: there's no way to get genre of a song...... wtf?
@@ -97,6 +132,12 @@ def print_items(items):
                 print(str(elem) + ": " + str(item[elem]))
         print()
 
+#returns number of songs on first playlist, can change this to certain playlist by using ID instead
+def count_songs_on_playlist():
+    results = sp.current_user_playlists(limit = 1)
+    num_songs = results['items'][0]['tracks']['total']
+    return num_songs
+
 #returns list of playlist ids
 def get_items(items):
     ids = []
@@ -129,11 +170,16 @@ def audio_features_stats(id):
 
 def stats_magic(t_ids):
     data = audio_features_stats(t_ids)
+    magic_numbers = {}
     for attr in data.keys():
         if attr not in ["loudness", "key", "mode", "time_signature", "instrumentalness"]: 
             #can't use harmonic mean for these. int avg/range for key, mode for mode and t_s
-            print(attr, data[attr], st.harmonic_mean(data[attr]))
-            print()
+            magic_numbers[attr] = st.harmonic_mean(data[attr])
+            #print(attr, data[attr], st.harmonic_mean(data[attr]))
+            #print()
+    for (k,v) in magic_numbers.items():
+        print(k + ":", v)
+    print()
 
 # 
 # audio analysis for project - titles include 
@@ -163,7 +209,7 @@ def audio_analysis(id):
 
 def audio_features(id):
     results = sp.audio_features(id)
-    print(type(results))
+    #print(type(results))
     for idx, audio in enumerate(results):
         print(audio)
 # can train against artist_related_artists(artist_id)? 
@@ -172,7 +218,8 @@ def audio_features(id):
 
 def main():
     #playlist_tracks()
-    get_common_related_artists(playlist_artists())
+    get_common_related_artists(weighted_playlist_artists())
+    #weighted_playlist_artists()
     #print(sp.recommendation_genre_seeds())
     #playlist()
     #songs()
